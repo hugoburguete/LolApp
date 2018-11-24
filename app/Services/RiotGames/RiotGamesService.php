1528df5c47804e2af7c891912a6e3f5304eabc93
Hugo Burguete <hugoburguete@hotmail.com>
2018-11-24 11:23:43 +0000

    Refactored service

--- app/Services/RiotGames/RiotGamesService.php
+++ app/Services/RiotGames/RiotGamesService.php
@@ -8,11 +8,24 @@ use LolApplication\Library\RiotGames\Resources\LeagueResource;
 class RiotGamesService implements RiotGamesInterface
 {
     /**
+     * Riot's Summoner Library Resource
+     * @var SummonerResource
+     */
+    private $summonerResource;
+
+    /**
+     * Riot's League Library Resource
+     * @var LeagueResource
+     */
+    private $leagueResource;
+
+    /**
      * Constructor
      */
     public function __construct()
     {
-
+        $this->summonerResource = resolve(SummonerResource::class);
+        $this->leagueResource = resolve(LeagueResource::class);
     }
 
     /**
@@ -20,9 +33,9 @@ class RiotGamesService implements RiotGamesInterface
      */
     public function getSummoner(string $summonerName): Summoner
     {
-        $summoner = resolve(SummonerResource::class)
+        $summoner = $this->summonerResource
             ->getSummoner($summonerName);
-        $positions = resolve(LeagueResource::class)
+        $positions = $this->leagueResource
             ->getPositionBySummoner($summoner);
         $summoner->addPositions($positions);
 
