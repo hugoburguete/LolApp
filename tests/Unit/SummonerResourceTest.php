76f446480f37d7469e2153197c8e1896287806e2
Hugo Burguete <hugoburguete@hotmail.com>
2018-12-09 13:03:58 +0000

    Cleaning up some of the tests

--- tests/Unit/SummonerResourceTest.php
+++ tests/Unit/SummonerResourceTest.php
@@ -5,10 +5,13 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
 use LolApplication\Services\RiotGames\RiotGamesService;
 
 class SummonerResourceTest extends TestCase
 {
+    use RefreshDatabase;
+
     public $serviceProvider;
 
     protected function setUp()
